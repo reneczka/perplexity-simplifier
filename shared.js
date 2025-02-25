4521a58a3f9077e9c6d10b76173eb744059049e2
reneczka <46569696+reneczka@users.noreply.github.com>
2025-02-25 22:53:23 +0100

    improve popup UI, hide library history and sources bar

--- shared.js
+++ shared.js
@@ -3,10 +3,20 @@ const defaultHiddenElements = [
   { selector: 'a[aria-label="Home"]', label: 'Home Section', enabled: true },
   { selector: 'a[aria-label="Discover"]', label: 'Discover Section', enabled: true },
   { selector: 'a[aria-label="Spaces"]', label: 'Spaces Section', enabled: true },
+  {
+    selector: 'a[aria-label="Library"], div.mb-md.mt-xs.space-y-xs.border-borderMain\\/50',
+    label: 'Library History',
+    enabled: true
+  },
   { selector: 'div.h-14.items-stretch', label: 'Download Bar', enabled: true },
   { selector: 'div.pb-md.hidden.md\\:block', label: 'Footer Links', enabled: true },
   { selector: 'div.absolute.w-full', label: 'News Section', enabled: true },
   { selector: 'div:has(> div > div > div > div.px-sm.py-1\\.5)', label: 'Pro Search Bar', enabled: true },
+  {
+    selector: 'div.mb-md.border-borderMain\\/50:has(div.scrollbar-none)',
+    label: 'Sources Section',
+    enabled: true
+  },
   { 
     selector: 'div:has(> div > div > svg.fa-images)', 
     label: 'Image Search Bar', 
